5d86fa158b23a296b9b3e8eb77d38d8204cc9b15
s1f102402439 <s1f102402439@iniad.org>
2026-02-18 02:50:47 +0900

    lendingのスペルミスをしていたのでそれをconfig/settings.pyもlendingフォルダも修正しました。

---
+++ lending/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import JsonResponse
+
+# 画面からのアクセスを受け取る関数
+def debt_list(request):
+    # 返したいデータ（辞書型）
+    data = [
+        {'id': 1, 'friend_name': '田中', 'amount': 1000, 'reason': 'ランチ代'},
+        {'id': 2, 'friend_name': '佐藤', 'amount': -500, 'reason': 'ジュース代'},
+    ]
