3997690aaa9075d4dd92b8bc2784a879c08d5ece
s1f102402439 <s1f102402439@iniad.org>
2026-03-09 23:55:54 +0900

    フロントエンドを作成していくためのライブラリとしてtailwindcssをダウンロードして利用できるようにしました。

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 👈 ①これを追加！
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 👈 ②これを追加！
+  ],
 })
